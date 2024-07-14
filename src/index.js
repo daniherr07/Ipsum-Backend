4fca012116092f11bce94eec8d2d271fa70c6250
maindream <dherrera2195@gmail.com>
2024-07-13 20:42:42 -0600

    cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://ipsum-alpha.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'https://ipsum-alpha.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.send();
+  });
 
 dotenv.config({path: './secret.env'})
 
